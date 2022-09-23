2f144a09c79d10c2e5f65584a4f73311a6469ae7
duhui1004@naver.com <duhui1004@naver.com>
2022-09-23 13:01:05 +0900

    react template 적용

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,30 @@
+import { useRoutes } from "react-router-dom";
+import Themeroutes from "./routes/Router";
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
-function App() {
-   const [hello, setHello] = useState('')
+const App = () => {
+  const routing = useRoutes(Themeroutes);
 
-    useEffect(() => {
-        axios.get('/todoList')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
+  return <div className="dark">{routing}</div>;
+};
 
-    return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
-    );
-}
+
+
+// function App() {
+//    const [hello, setHello] = useState('')
+
+//     useEffect(() => {
+//         axios.get('/todoList')
+//         .then(response => setHello(response.data))
+//         .catch(error => console.log(error))
+//     }, []);
+
+//     return (
+//         <div>
+//             백엔드에서 가져온 데이터입니다 : {hello}
+//         </div>
+//     );
+// }
 
 export default App;
