d5dac1c793d582010af146337157315f6cc72e79
duhui1004@naver.com <duhui1004@naver.com>
2022-09-26 21:01:15 +0900

    security config 추가

---
+++ src/main/java/com/project/scheduler/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.project.scheduler.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .mvcMatchers(
+                                HttpMethod.GET,
+                                "/"
+                                // "/login",
+                                // "/join"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin().and()
+                .logout()
+                        .logoutSuccessUrl("/")
+                        .and()
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    
+}
